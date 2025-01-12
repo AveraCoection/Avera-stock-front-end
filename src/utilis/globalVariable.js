4f012d53b2ba0b376fc488614687e12523987c3e
Sohail <sohail2637@gmail.com>
2025-01-12 18:37:23 +0500

    changed to live url

--- src/utilis/globalVariable.js
+++ src/utilis/globalVariable.js
@@ -1,7 +1,7 @@
 const GlobalApiState = {
   // DEV_BASE_LOCAL: "http://localhost:4000",
-  // DEV_BASE_LIVE: "https://avera-stock-back-end.vercel.app",
-  DEV_BASE_LIVE: "http://localhost:4000",
+  DEV_BASE_LIVE: "https://avera-stock-back-end.vercel.app",
+  // DEV_BASE_LIVE: "http://localhost:4000",
 
 };
 
